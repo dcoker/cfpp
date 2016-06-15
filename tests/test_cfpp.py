8fee01d138be7cc305395437b8feb92566619580
Doug Coker <dcoker@gmail.com>
2016-06-14 19:22:35 -0700

    Normalize JSON before running string comparisons.

--- tests/test_cfpp.py
+++ tests/test_cfpp.py
@@ -3,16 +3,18 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import base64
 import json
-import os
 import subprocess
 import unittest
 from glob import glob
 
-from cfpp.__main__ import camel_to_snake
+import os
+
+
+def json_normalize(s):
+    return json.dumps(json.loads(s), indent=2)
 
 
 class TestCfpp(unittest.TestCase):
-
     def test_files(self):
         self.maxDiff = None
         test_inputs = glob("tests/0*.template")
@@ -21,7 +23,7 @@ class TestCfpp(unittest.TestCase):
                 ["cfpp", "-s", "tests", one_input])
             with open(one_input.replace(".template", ".output.json"), "r") as fp:
                 expected = fp.read()
-            self.assertMultiLineEqual(expected, output)
+            self.assertMultiLineEqual(json_normalize(expected), json_normalize(output))
             print("%s passed." % one_input)
 
     def test_kms(self):
@@ -49,10 +51,27 @@ class TestCfpp(unittest.TestCase):
                     EncryptionContext={"ContextKey": "ContextValue"})
 
     def test_camel_to_snake(self):
+        from cfpp.extrinsics import camel_to_snake
         self.assertEquals("kms::encrypt_file",
                           camel_to_snake("KMS::EncryptFile"))
         self.assertEquals("mime_multipart", camel_to_snake("MimeMultipart"))
         self.assertEquals("trim", camel_to_snake("Trim"))
+
+    def test_is_ref(self):
+        from cfpp.extrinsics import is_ref
+        self.assertTrue(is_ref({"Ref": "R"}))
+        self.assertTrue(is_ref({"ref": "R"}))
+        self.assertTrue(is_ref({"REF": "R"}))
+        self.assertFalse(is_ref(None))
+        self.assertFalse(is_ref([]))
+        self.assertFalse(is_ref({}))
+        self.assertFalse(is_ref({"X": "Y"}))
+        self.assertFalse(is_ref({"X": "Y", "Z": "0"}))
+
+    def test_walk_var_substitution(self):
+        from cfpp.extrinsics import walk
+        self.assertEqual("ex", walk({"Ref": "X"}, {}, "root", {"X": "ex"}))
+        self.assertEqual({"O": "ex"}, walk({"O": {"Ref": "X"}}, {}, ["root"], {"X": "ex"}))
 
 
 if __name__ == '__main__':
